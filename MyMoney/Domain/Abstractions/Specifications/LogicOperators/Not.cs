890bc8edbcb9b2e8e5623126d9bd80c211c13b35
André Pires <vbzero@hotmail.com>
2015-09-02 14:04:30 -0300

    Including the Domain Abstractions Project

---
+++ MyMoney/Domain/Abstractions/Specifications/LogicOperators/Not.cs
@@ -0,0 +1,23 @@
+﻿namespace MyMoney.Domain.Abstractions.Specifications.LogicOperators
+{
+    internal class Not<TEntity> : CompositeSpecification<TEntity>
+    {
+        public override string Name
+        {
+            get { return "Not Logic Between Specifications"; }
+        }
+
+        private ISpecification<TEntity> Wrapped { get; set; }
+
+        public Not(ISpecification<TEntity> wrapped)
+        {
+            Wrapped = wrapped;
+        }
+
+        public override bool IsSatisfiedBy(TEntity entity)
+        {
+            return !Wrapped.IsSatisfiedBy(entity);
+        }
+
+    }
+}
