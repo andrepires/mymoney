a7d34e92e9fbf9574bd3964bbf9d78c236928b05
André Pires <vbzero@hotmail.com>
2015-09-15 11:51:02 -0300

    some little changes

--- MyMoney/Presentation/AngularSPA/app/config.route.js
+++ MyMoney/Presentation/AngularSPA/app/config.route.js
@@ -3,31 +3,6 @@
 
     var app = angular.module('app');
 
-    // Collect the routes
-    app.constant('routes', getRoutes());
-    
-    // Configure the routes and route resolvers
-    app.config(['$routeProvider', 'routes', routeConfigurator]);
-    function routeConfigurator($routeProvider, routes) {
-
-        routes.forEach(function (r) {
-            setRoute(r.url, r.config);
-        });
-        $routeProvider.otherwise({ redirectTo: '/' });
-
-        function setRoute(url, definition) {
-            definition.resolve = angular.extend(definition.resolve || {}, {
-                prime: prime
-            });
-            $routeProvider.when(url, definition);
-            return $routeProvider;;
-        }
-    }
-
-    prime.$inject = ["datacontext"];
-
-    function prime(dc) { return dc.prime(); }
-
     // Define the routes 
     function getRoutes() {
         return [
@@ -87,4 +62,31 @@
             }
         ];
     }
+    
+    function routeConfigurator($routeProvider, routes) {
+
+        routes.forEach(function (r) {
+            setRoute(r.url, r.config);
+        });
+        $routeProvider.otherwise({ redirectTo: '/' });
+
+        function setRoute(url, definition) {
+            definition.resolve = angular.extend(definition.resolve || {}, {
+                prime: prime
+            });
+            $routeProvider.when(url, definition);
+            return $routeProvider;;
+        }
+    }
+
+    function prime(datacontext) { return datacontext.prime(); }
+
+    // Collect the routes
+    app.constant('routes', getRoutes());
+    
+    // Configure the routes and route resolvers
+    app.config(['$routeProvider', 'routes', routeConfigurator]);
+
+    prime.$inject = ["datacontext"];
+    
 })();
